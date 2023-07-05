66f104aad9e8b79c8c31240fc2bf1300bd31843f
Deepika <deepikardb@gmail.com>
2023-07-05 14:15:02 -0700

    Initial Commit

---
+++ MessengerSwiftUI/MessengerSwiftUIApp.swift
@@ -0,0 +1,19 @@
+//
+//  MessengerSwiftUIApp.swift
+//  MessengerSwiftUI
+//
+//  Created by Deepika Ramesh on 7/5/23.
+//
+
+
+
+import SwiftUI
+
+@main
+struct MessengerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
